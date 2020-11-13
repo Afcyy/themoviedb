12c0833da06590e52dfa36460cf466e50d6347a0
Afcyy <vaptsiauri@top.ge>
2020-11-14 01:06:35 +0400

    Heroku

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crocodile | Movies</title>
 
-    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
     @livewireStyles
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
